f88599f0c3abf33df43d406bc00d9c8adf75297d
JEONG HYEON OH <52736095+zzocojoa@users.noreply.github.com>
2024-02-27 22:43:00 +0900

    DataList save module modify

--- client/src/components/DataListUI.js
+++ client/src/components/DataListUI.js
@@ -11,6 +11,8 @@ function DataListUI() {
   const [selectedItems, setSelectedItems] = useState([]);
   const [displayCount, setDisplayCount] = useState(10);
   const [searchTerm, setSearchTerm] = useState('');
+  const [isHandleSaveCsvChecked, setIsHandleSaveCsvChecked] = useState(true);
+  const csvSaveModeText = isHandleSaveCsvChecked ? "개별" : "복수";
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -76,31 +78,64 @@ function DataListUI() {
 
   // DB CSV data save
   const handleSaveCsv = async () => {
-    // 선택된 항목의 상세 정보를 비동기적으로 가져옵니다.
-    const selectedDataDetails = await Promise.all(selectedItems.map(id => fetchDataDetails(id)));
+    if (isHandleSaveCsvChecked) {
+      const selectedDataDetails = await Promise.all(selectedItems.map(id => fetchDataDetails(id)));
 
-    // CSV 헤더
-    const csvHeader = 'filedate,countNumber,wNumber,dwNumber,dieNumber,median, startTime, endTime\n';
+      selectedDataDetails.forEach((item) => {
+        // CSV 헤더
+        const csvHeader = 'Date,Time,Temperature\n';
 
-    // 각 항목을 CSV 형식의 문자열로 변환
-    const csvRows = selectedDataDetails.map(item => {
-      const { filedate, numbering: { countNumber, wNumber, dwNumber, dieNumber }, boxplotStats: { median }, startTime, endTime } = item;
-      return `"${filedate}","${countNumber}","${wNumber}","${dwNumber}","${dieNumber}","${median}","${startTime}","${endTime}"`;
-    });
+        // 각 항목의 temperatureData를 CSV 형식의 문자열로 변환
+        const csvRows = item.temperatureData.map(tempData => {
+          const { Date, Time, Temperature } = tempData;
+          return `"${Date}","${Time}","${Temperature}"`;
+        }).join("\n");
 
-    // CSV 헤더와 모든 행을 결합하여 최종 CSV 내용을 생성
-    const csvContent = `data:text/csv;charset=utf-8,${csvHeader}${csvRows.join("\n")}`;
+        // 파일명 생성 로직 수정: 각 항목에 대한 고유 파일명 생성
+        const { filedate, numbering: { countNumber, wNumber, dwNumber, dieNumber } } = item;
+        // 파일명을 생성하는 부분을 forEach 루프 내부에 넣어 각 항목별로 고유한 이름을 가지도록 함
+        const csvFileName = `${filedate}-${countNumber}_${wNumber}_${dwNumber}_${dieNumber}`.replace(/\/|:|\s/g, '_'); // 파일명에 사용 불가능한 문자는 '_'로 대체
 
-    // encodeURI를 사용하여 CSV 내용에 대한 URI를 생성
-    const encodedUri = encodeURI(csvContent);
+        // CSV 헤더와 모든 행을 결합하여 최종 CSV 내용을 생성
+        const csvContent = `data:text/csv;charset=utf-8,${csvHeader}${csvRows}`;
+        const encodedUri = encodeURI(csvContent);
 
-    // 임시 링크를 생성하여 프로그램적으로 클릭 이벤트를 발생시켜 파일 다운로드를 트리거
-    const link = document.createElement('a');
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', 'selected_data.csv'); // 다운로드될 파일명 설정
-    document.body.appendChild(link); // 링크를 문서에 추가
-    link.click(); // 링크 클릭 이벤트 강제 실행
-    document.body.removeChild(link); // 사용 후 링크 제거
+        // 임시 링크를 생성하여 프로그램적으로 클릭 이벤트를 발생시켜 파일 다운로드를 트리거
+        const link = document.createElement('a');
+        link.setAttribute('href', encodedUri);
+        link.setAttribute('download', `${csvFileName}.csv`); // 수정된 파일명 사용
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+
+    } else {
+      // 선택된 항목의 상세 정보를 비동기적으로 가져옵니다.
+      const selectedDataDetails = await Promise.all(selectedItems.map(id => fetchDataDetails(id)));
+
+      // CSV 헤더
+      const csvHeader = 'filedate,countNumber,wNumber,dwNumber,dieNumber,median, startTime, endTime\n';
+
+      // 각 항목을 CSV 형식의 문자열로 변환
+      const csvRows = selectedDataDetails.map(item => {
+        const { filedate, numbering: { countNumber, wNumber, dwNumber, dieNumber }, boxplotStats: { median }, startTime, endTime } = item;
+        return `"${filedate}","${countNumber}","${wNumber}","${dwNumber}","${dieNumber}","${median}","${startTime}","${endTime}"`;
+      });
+
+      // CSV 헤더와 모든 행을 결합하여 최종 CSV 내용을 생성
+      const csvContent = `data:text/csv;charset=utf-8,${csvHeader}${csvRows.join("\n")}`;
+
+      // encodeURI를 사용하여 CSV 내용에 대한 URI를 생성
+      const encodedUri = encodeURI(csvContent);
+
+      // 임시 링크를 생성하여 프로그램적으로 클릭 이벤트를 발생시켜 파일 다운로드를 트리거
+      const link = document.createElement('a');
+      link.setAttribute('href', encodedUri);
+      link.setAttribute('download', 'selected_data.csv'); // 다운로드될 파일명 설정
+      document.body.appendChild(link); // 링크를 문서에 추가
+      link.click(); // 링크 클릭 이벤트 강제 실행
+      document.body.removeChild(link); // 사용 후 링크 제거
+    }
   };
 
   return (
@@ -116,13 +151,21 @@ function DataListUI() {
         />
       </div>
       <div className={styles['selectAllCheckbox']}>
-        <label className={styles['selectAllLabel']}>
+        <div className={styles['selectAllLabel']}>
           <input
             type="checkbox"
             checked={selectedItems.length === filteredDataList.length && filteredDataList.length > 0}
             onChange={handleSelectAllChange}
           />전체 선택
-        </label>
+        </div>
+        <div className={styles['selectAllLabel']}>
+          <input
+            type="checkbox"
+            checked={isHandleSaveCsvChecked}
+            onChange={e => setIsHandleSaveCsvChecked(e.target.checked)}
+          />
+          <label>{`저장 유형 선택: ${csvSaveModeText}`}</label>
+        </div>
       </div>
       <div className={`${styles['DataListContainer']} ${styles['scroll']} ${styles['scroll-css']}`}>
         {filteredDataList.slice(0, displayCount).map((dataItem, index) => (
