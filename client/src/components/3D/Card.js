c6dc1eee64325a8c250ad6a27567cc758f1b0a92
JEONG HYEON OH <52736095+zzocojoa@users.noreply.github.com>
2024-03-08 01:24:03 +0900

    3d card modify

--- client/src/components/3D/Card.js
+++ client/src/components/3D/Card.js
@@ -7,13 +7,20 @@ import styles from './Card.module.css';
 function Card({ selectedImage }) {
     const cardRef = useRef();
     const frameId = useRef(null);
+    const glossEffectRef = useRef(null);
 
     useEffect(() => {
         const card = cardRef.current;
-        let overlay = card.querySelector('.' + styles['pokemon-image-overlay']);
+        let overlay = card.querySelector('.' + styles['imageOverlay']);
         let mouseX = 0;
         let mouseY = 0;
         const sensitivity = 15;
+
+        // 광택 효과 요소 생성 및 스타일 설정
+        let glossEffect = document.createElement('div');
+        glossEffect.className = styles['gloss-effect']; // CSS 모듈 사용 시 클래스 이름 수정
+        card.appendChild(glossEffect);
+        glossEffectRef.current = glossEffect;
 
         const updateOverlay = (e) => {
             if (!overlay) return; // overlay 존재 확인
@@ -34,7 +41,7 @@ function Card({ selectedImage }) {
             const rotateY = mouseX * sensitivity; // Y축 회전 값
 
             // 카드 스타일에 회전 적용
-            card.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) translateZ(30px)`;
+            card.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) translateZ(40px)`;
 
             // 실시간 변환을 위해 트랜지션을 'none'으로 설정
             card.style.transition = 'none';
@@ -45,6 +52,12 @@ function Card({ selectedImage }) {
 
             // 마우스 X 좌표에 따라 카드의 투명도 조정
             card.style.opacity = 1.7 - Math.abs(mouseX);
+
+            // 광택 효과 위치 업데이트
+            const glossX = e.clientX - cardRect.left;
+            const glossY = e.clientY - cardRect.top;
+            glossEffect.style.left = `${glossX - glossEffect.offsetWidth / 12}px`;
+            glossEffect.style.top = `${glossY - glossEffect.offsetHeight / 11}px`;
         };
 
         card.addEventListener('mousemove', handleMouseMove);
@@ -55,7 +68,7 @@ function Card({ selectedImage }) {
         };
         const handleMouseLeave = () => {
             // overlay 존재 확인
-            if (!overlay) overlay = card.querySelector('.' + styles['pokemon-image-overlay']);
+            if (!overlay) overlay = card.querySelector('.' + styles['imageOverlay']);
             // overlay가 존재할 때만 스타일 적용
             if (overlay) {
                 // 마우스 리브 시 전환 효과 설정
@@ -66,8 +79,10 @@ function Card({ selectedImage }) {
                 cancelAnimationFrame(frameId.current);
                 overlay.style.backgroundPosition = 'initial';
             }
-             // 마우스가 벗어났을 때 원래 트랜지션으로 복구
+            // 마우스가 벗어났을 때 원래 트랜지션으로 복구
             card.style.transition = 'transform 0.5s ease-out, opacity 0.5s ease-out';
+            glossEffect.style.left = '50%';
+            glossEffect.style.top = '50%';
         };
 
         // 마우스 엔터와 리브 이벤트 리스너 추가
@@ -79,17 +94,21 @@ function Card({ selectedImage }) {
             card.removeEventListener('mousemove', handleMouseMove);
             card.removeEventListener('mouseenter', handleMouseEnter);
             card.removeEventListener('mouseleave', handleMouseLeave);
+            card.removeChild(glossEffect);
             cancelAnimationFrame(frameId.current);
         };
     }, []);
 
     // 카드 컴포넌트 렌더링
     return (
-        <div className={styles["card-container"]}>
-            <div className={styles["pokemon-card"]} ref={cardRef}>
-                <div alt="Pokemon" className={styles["pokemon-image-overlay"]}></div>
-                <img src={selectedImage} alt="Pokemon" className={styles["pokemon-image"]} />
-                <h2 className={styles["pokemon-name"]}>Jeong-Hyeon Oh</h2>
+        <div className={styles["cardWrap"]}>
+            <div className={styles["cardContainer"]} ref={cardRef}>
+                <div alt="image-title" className={styles["imageOverlay"]}></div>
+                <img src={selectedImage} alt="image-title" className={styles["imageTitle"]} />
+                <div className={styles["titleWrpa"]}>
+                    <span className={styles["titleName"]}>Developer</span><br></br>
+                    <span>Jeong-Hyeon Oh</span>
+                </div>
             </div>
         </div>
     );
