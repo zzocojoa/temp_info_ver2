5397ed0cbc7d778042e5244e3e1c559cfb2229dd
JEONG HYEON OH <52736095+zzocojoa@users.noreply.github.com>
2024-04-05 14:25:26 +0900

    banner logic modify

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -16,10 +16,10 @@ function Banner() {
     };
 
     const toggleSidebar = () => {
-        console.log('Toggling sidebar. Current state is:', isOpen); // For debugging
+        console.log('Toggling sidebar. Current state is:', isOpen);
         setIsOpen(!isOpen);
     };
-    
+
     const getSpanClassName = (baseClass, positionClass) => {
         let classes = [styles[baseClass], styles[positionClass]];
         if (isOpen) classes.push(styles['menuSpanActive']);
@@ -29,51 +29,52 @@ function Banner() {
     return (
         <div className={styles.bannerWrap}>
             <input type="checkbox" id="menuicon" className={styles.menuIcon} onChange={toggleSidebar} checked={isOpen} />
-            <label htmlFor="menuicon" className={styles.menuLabel}>
-                <span className={getSpanClassName('menuSpan', 'menuSpanFirst')}></span>
-                <span className={`${getSpanClassName('menuSpan', 'menuSpanMiddle')} ${isOpen ? styles.hidden : ''}`}></span>
-                <span className={getSpanClassName('menuSpan', 'menuSpanLast')}></span>
-            </label>
+            <div className={styles['bannerlogoWrap']}>
+                <label htmlFor="menuicon" className={styles.menuLabel}>
+                    <span className={getSpanClassName('menuSpan', 'menuSpanFirst')}></span>
+                    <span className={`${getSpanClassName('menuSpan', 'menuSpanMiddle')} ${isOpen ? styles.hidden : ''}`}></span>
+                    <span className={getSpanClassName('menuSpan', 'menuSpanLast')}></span>
+
+                </label>
+                <div className={styles['banner-logo']} onClick={() => navigateTo('/')} style={{ cursor: 'pointer' }}>
+                        {isOpen && <img src="./images/logo.png" alt="logo" />}
+                </div>
+            </div>
+
             <div className={`${styles.sidebar} ${isOpen ? styles.sidebarOpen : ''}`}>
                 <div className={styles['bannerContainer']}>
                     <div className={styles['banner']}>
-                        <div onClick={() => navigateTo('/')} style={{ cursor: 'pointer' }}>
-                            <div className={styles['banner-logo']}>
-                            {/* {isOpen && <img src="./images/logo.png" alt="logo" />} */}
-                            </div>
-                        </div>
                         <ul className={styles['banner-menu']}>
                             <li className={styles['banner-title']} onClick={() => navigateTo('/graph-data')} style={{ cursor: 'pointer' }}>
                                 <div className={styles['icon-img']} style={{ color: 'orange' }}>
                                     <FontAwesomeIcon icon={faSignal} />
                                 </div>
-                                {/* Debugging line below */}
-                                {isOpen ? <div>Temp.Graph</div> : <div style={{ display: 'none' }}>Temp.Graph</div>}
+                                {isOpen ? <div className={styles['banner-title-name']}>T.Graph</div> : null}
                             </li>
                             <li className={styles['banner-title']} onClick={() => navigateTo('/line-bar')} style={{ cursor: 'pointer' }}>
                                 <div className={styles['icon-img']}>
                                     <FontAwesomeIcon icon={faChartLine} />
                                 </div>
-                                {isOpen ? <div>Line/Bar</div> : <div style={{ display: 'none' }}>Line/Bar</div>}
+                                {isOpen ? <div className={styles['banner-title-name']}>Line/Bar</div> : null}
                             </li>
                             <li className={styles['banner-title']} onClick={() => navigateTo('/Analysis-page')} style={{ cursor: 'pointer' }}>
                                 <div className={styles['icon-img']}>
                                     <FontAwesomeIcon icon={faMagnifyingGlassChart} />
                                 </div>
-                                {isOpen ? <div>Analysis</div> : <div style={{ display: 'none' }}>Analysis</div>}
+                                {isOpen ? <div className={styles['banner-title-name']}>Analysis</div> : null}
                             </li>
                             <li className={styles['banner-title']} onClick={() => navigateTo('/graph-data')} style={{ cursor: 'pointer' }}>
                                 <div className={styles['icon-img']}>
-                                    {/* Placeholder for future icon */}
+                                    <FontAwesomeIcon icon={faMagnifyingGlassChart} />
                                 </div>
-                                {isOpen ? <div>준비중...</div> : <div style={{ display: 'none' }}>준비중...</div>}
+                                {isOpen ? <div className={styles['banner-title-name']}>준비중...</div> : null}
                             </li>
                         </ul>
                         <ul className={styles['banner-icons']}>
                             <li>
-                            {isOpen && <a href='https://github.com/zzocojoa/temp_info_ver2' aria-label="GitHub" target='_blank' rel="noreferrer noopener">
+                                <a href='https://github.com/zzocojoa/temp_info_ver2' aria-label="GitHub" target='_blank' rel="noreferrer noopener">
                                     <FontAwesomeIcon icon={faGithub} />
-                                </a>}
+                                </a>
                             </li>
                             <li onClick={() => navigateTo('/Card')} style={{ cursor: 'pointer' }}>
                                 {/* <img className={styles['developer-icon']} src="./images/hoihou-icon-1.png" alt="hoihou-icon-1" /> */}
