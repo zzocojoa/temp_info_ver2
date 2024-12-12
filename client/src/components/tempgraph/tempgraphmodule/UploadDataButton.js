45b3dc94f493a7167ee85e8b719a5325af1687c4
JEONG HYEON OH <52736095+zzocojoa@users.noreply.github.com>
2024-12-12 11:25:32 +0900

    그래프 생성 진행률 시각화

--- client/src/components/tempgraph/tempgraphmodule/UploadDataButton.js
+++ client/src/components/tempgraph/tempgraphmodule/UploadDataButton.js
@@ -1,26 +1,35 @@
-// client/src/components/tempgraph/tempgraphmodule/UploadDataButton.js
-
 import React, { useState } from 'react';
 import styles from './UploadDataButton.module.css'
 import { uploadFile } from '../../../api';
-import Loader from './Loader';
+import ProgressBar from './ProgressBar';
 
 function UploadDataButton({ selectedFile, onUploadSuccess, isEnabled }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const handleProgress = (event) => {
+    if (event.lengthComputable) {
+      const percentComplete = Math.round((event.loaded / event.total) * 100);
+      setProgress(percentComplete);
+    }
+  };
 
   const handleUpload = async () => {
-
     if (!selectedFile) {
       alert('Please select a file first.');
       return;
     }
 
     setIsLoading(true);
+    setProgress(0);
 
     try {
       // API를 호출하여 파일 업로드
-      const response = await uploadFile(selectedFile);
-      setIsLoading(false); // 업로드 성공 또는 실패 시 로딩 상태를 false로 설정
+      const response = await uploadFile(selectedFile, handleProgress);
+      
+      setIsLoading(false);
+      setProgress(100);
+
       if (response) {
         const { averagedData, boxplotStats } = response;
         const startTime = averagedData[0]?.time || '';
@@ -31,6 +40,7 @@ function UploadDataButton({ selectedFile, onUploadSuccess, isEnabled }) {
       }
     } catch (error) {
       setIsLoading(false);
+      setProgress(0);
       console.error('Error uploading file:', error);
       alert('Error uploading file.');
     }
@@ -39,9 +49,13 @@ function UploadDataButton({ selectedFile, onUploadSuccess, isEnabled }) {
   return (
     <>
       {isLoading ? (
-        <Loader />
+        <ProgressBar percentage={progress} />
       ) : (
-        <button className={styles['UploadDataButton']} onClick={handleUpload} disabled={!isEnabled || isLoading}>
+        <button 
+          className={styles['UploadDataButton']} 
+          onClick={handleUpload} 
+          disabled={!isEnabled || isLoading}
+        >
           그래프 생성
         </button>
       )}
